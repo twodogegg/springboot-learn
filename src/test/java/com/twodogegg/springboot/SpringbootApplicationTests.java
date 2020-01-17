54d0e0b57d83f55ba67e54877b6f1637c9e85924
shuliuzhenhua <971270272@qq.com>
2020-01-17 13:57:50 +0800

    init

---
+++ src/test/java/com/twodogegg/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,11 @@
+package com.twodogegg.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringbootApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
